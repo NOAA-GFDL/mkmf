f677d680d8776aec09ff4d37a6000c6e0771b325
Chris Blanton <Chris.Blanton@ldt-4359643.gfdl.noaa.gov>
2021-11-04 09:54:09 -0400

    Minor formatting changes for consistency

--- templates/ncrc-intel.mk
+++ templates/ncrc-intel.mk
@@ -46,9 +46,9 @@ NETCDF =             # If value is '3' and CPPDEFS contains
                      # '-Duse_netCDF', then the additional cpp macro
                      # '-Duse_LARGEFILE' is added to the CPPDEFS macro.
 
-INCLUDES = -I/sw/gaea-cle7/uasw/ncrc/envs/20200417/opt/linux-sles15-x86_64/gcc-7.5.0/libyaml-0.2.5-sfyudki4c5n5xusvwxmp3bdd6ra5z3lq/include/
-          # A list of -I Include directories to be added to the
+                     # A list of -I Include directories to be added to the
                      # the compile command.
+INCLUDES = -I/sw/gaea-cle7/uasw/ncrc/envs/20200417/opt/linux-sles15-x86_64/gcc-7.5.0/libyaml-0.2.5-sfyudki4c5n5xusvwxmp3bdd6ra5z3lq/include
 
 ISA = -xsse2         # The Intel Instruction Set Archetecture (ISA) compile
                      # option to use.  If blank, than use the default SSE
@@ -134,6 +134,8 @@ LDFLAGS_VERBOSE := -Wl,-V,--verbose,-cref,-M
 LDFLAGS_COVERAGE = -prof-gen=srcpos
 
 # Start with blank LIBS
+LIBS :=
+# Add library location for libyaml
 LIBS := -L/sw/gaea-cle7/uasw/ncrc/envs/20200417/opt/linux-sles15-x86_64/gcc-7.5.0/libyaml-0.2.5-sfyudki4c5n5xusvwxmp3bdd6ra5z3lq/lib -lyaml
 
 # Get compile flags based on target macros.
