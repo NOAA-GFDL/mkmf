882c83f226744d93fc78cd38533e0d4b67948633
Chris Blanton <chris.blanton@noaa.gov>
2022-07-07 16:09:28 -0400

    #42 For intel and intel-oneapi, set HAVE_GETTID macro as C5's environment contains its own gettid Linux system call available in the GNU standard library. The intel-classic environment does not have the gettid system function defined.
    More details: https://github.com/NOAA-GFDL/FMS/issues/276
    
    The error one sees is:
    src/FMS/affinity/affinity.c:55:14: error: static declaration of 'gettid' follows non-static declaration
    static pid_t gettid(void)
                 ^
    /usr/include/bits/unistd_ext.h:34:16: note: previous declaration is here
    extern __pid_t gettid (void) __THROW;
                   ^
    1 error generated.

--- templates/ncrc5-intel.mk
+++ templates/ncrc5-intel.mk
@@ -83,7 +83,7 @@ MAKEFLAGS += --jobs=$(shell grep '^processor' /proc/cpuinfo | wc -l)
 CPPDEFS += -Duse_netCDF
 
 # Additional Preprocessor Macros needed due to  Autotools and CMake
-CPPDEFS += -DHAVE_SCHED_GETAFFINITY
+CPPDEFS += -DHAVE_SCHED_GETAFFINITY -DHAVE_GETTID
 
 # Macro for Fortran preprocessor
 FPPFLAGS := -fpp -Wp,-w $(INCLUDES)
